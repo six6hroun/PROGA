574e3a33391e28952cfd45e362be828fd491c5e1
six6hroun <gura0516@gmail.com>
2024-11-19 21:28:06 +0300

    Update Program.cs

--- lab9/Program.cs
+++ lab9/Program.cs
@@ -1,11 +1,43 @@
-﻿using System;
-using stek2;
+using System;
+using stek;
 using MyVector;
+using System.Collections.Generic;
+using System.Numerics;
 namespace Lab9
 {
     class Program
     {
-        static public int Priority(string operation)
+        static void Main(string[] args)
+        {
+            Console.Write("Введите математическое выражение: ");
+            string primer = Console.ReadLine();
+            Console.WriteLine(Calc(primer));
+        }
+
+        static private double InitOperaci(string operation, params double[] x) => operation switch
+        {
+            "+" => x[0] + x[1],
+            "-" => x[0] - x[1],
+            "*" => x[0] * x[1],
+            "/" => x[0] / x[1],
+            "^" => Math.Pow(x[0], x[1]),
+            "koren" => Math.Sqrt(x[0]),
+            "abs" => Math.Abs(x[0]),
+            "sin" => Math.Sin(x[0]),
+            "cos" => Math.Cos(x[0]),
+            "tg" => Math.Tan(x[0]),
+            "ln" => Math.Log(x[0]),
+            "log" => Math.Log10(x[0]),
+            "min" => x[0] < x[1] ? x[0] : x[1],
+            "max" => x[0] > x[1] ? x[0] : x[1],
+            "mod" => (int)x[1] % (int)x[0],
+            "//" => Math.Floor(x[0] / x[1]),
+            "exp" => Math.Exp(x[0]),
+            "trunc" => Math.Truncate(x[0]),
+            _ => throw new Exception("Неизвестная операция")
+        };
+
+        static public int Prioritet(string operation)
         {
             switch (operation)
             {
@@ -18,12 +50,11 @@ namespace Lab9
                     return 2;
                 case "^":
                     return 3;
-                case "sqrt":
+                case "koren":
                 case "ln":
                 case "cos":
                 case "sin":
                 case "tg":
-                case "ctg":
                 case "abs":
                 case "log":
                 case "min":
@@ -31,36 +62,11 @@ namespace Lab9
                 case "mod":
                 case "exp":
                 case "trunc":
-                case "pi":
                     return 4;
                 default: return 0;
             }
         }
 
-        static private double InitOp(string operation, params double[] x) => operation switch
-        {
-            "+" => x[0] + x[1],
-            "-" => x[0] - x[1],
-            "*" => x[0] * x[1],
-            "/" => x[0] / x[1],
-            "//" => Math.Floor(x[0] / x[1]),
-            "^" => Math.Pow(x[0], x[1]),
-            "exp" => Math.Exp(x[0]),
-            "sqrt" => Math.Sqrt(x[0]),
-            "ln" => Math.Log(x[0]),
-            "log" => Math.Log10(x[0]),
-            "cos" => Math.Cos(x[0]),
-            "sin" => Math.Sin(x[0]),
-            "tg" => Math.Tan(x[0]),
-            "ctg" => 1 / Math.Tan(x[0]),
-            "abs" => Math.Abs(x[0]),
-            "min" => x[0] < x[1] ? x[0] : x[1],
-            "max" => x[0] > x[1] ? x[0] : x[1],
-            "mod" => (int)x[0] % (int)x[1],
-            "truncate" => Math.Truncate(x[0]),
-            "pi" => Math.PI,
-            _ => throw new Exception("Неизвестная операция")
-        };
         static private string Tmp(string expression)
         {
             MyVector<string> vectorOfItem = new MyVector<string>();
@@ -76,12 +82,11 @@ namespace Lab9
                 {
                     switch (item)
                     {
-                        case "sqrt":
+                        case "koren":
                         case "ln":
                         case "cos":
                         case "sin":
                         case "tg":
-                        case "ctg":
                         case "abs":
                         case "log":
                         case "min":
@@ -89,7 +94,6 @@ namespace Lab9
                         case "mod":
                         case "exp":
                         case "trunc":
-                        case "pi":
                             break;
                         default:
                             vectorOfItem.Add(item);
@@ -100,13 +104,13 @@ namespace Lab9
 
             for (int i = 0; i < vectorOfItem.Size(); i++)
             {
-                Console.WriteLine($"Input tmp {vectorOfItem.Get(i)}: ");
+                Console.WriteLine($"Введите значение {vectorOfItem.Get(i)}: ");
                 expression = expression.Replace(vectorOfItem.Get(i), Console.ReadLine());
             }
             return expression;
         }
 
-        static public MyVector<string> Polska(string expression)
+        static public MyVector<string> PolskNotacia(string expression)
         {
             expression = Tmp(expression);
             MyStack<string> stack = new MyStack<string>();
@@ -144,7 +148,7 @@ namespace Lab9
                     if (stack.Empty()) { stack.Push(expression[i].ToString()); }
                     else
                     {
-                        while (!stack.Empty() && (Priority(stack.Peek()) > Priority(expression[i].ToString())))
+                        while (!stack.Empty() && (Prioritet(stack.Peek()) > Prioritet(expression[i].ToString())))
                         {
                             string b = stack.Peek();
                             result.Add(b.ToString());
@@ -183,7 +187,7 @@ namespace Lab9
         static public double Calc(string expression)
         {
             if (expression == null) throw new Exception("Пусто");
-            MyVector<string> postfixForm = Polska(expression);
+            MyVector<string> postfixForm = PolskNotacia(expression);
             MyStack<double> stack = new MyStack<double>();
 
             char[] basicOperator = new char[] { '+', '-', '*', '^', '/' };
@@ -205,13 +209,13 @@ namespace Lab9
                         stack.Pop();
                         double number2 = stack.Peek();
                         stack.Pop();
-                        stack.Push(InitOp(element, number1, number2));
+                        stack.Push(InitOperaci(element, number1, number2));
                     }
                     else
                     {
                         double number = stack.Peek();
                         stack.Pop();
-                        stack.Push(InitOp(element, number));
+                        stack.Push(InitOperaci(element, number));
                     }
                 }
                 else if (basicOperator.Contains(element[0]))
@@ -225,20 +229,12 @@ namespace Lab9
                     stack.Pop();
                     double number1 = stack.Peek();
                     stack.Pop();
-                    stack.Push(InitOp(element, number1, number2));
+                    stack.Push(InitOperaci(element, number1, number2));
                 }
             }
             double answer = stack.Peek();
             stack.Pop();
             return answer;
-        }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Введите математическое выражение");
-            Console.WriteLine();
-            string example = Console.ReadLine();
-            Console.WriteLine(Calc(example));
         }
     }
 }
